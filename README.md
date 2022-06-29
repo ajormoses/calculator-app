708626dfc2c3ea49f2e3294492a94de6dd05a33a
ajormoses <ajormoses@gmail.com>
2022-06-29 18:01:34 +0100

    completed

---
+++ README.md
@@ -0,0 +1,31 @@
+# Frontend Mentor - Calculator app
+
+![Design preview for the Calculator app coding challenge](./design/desktop-preview.jpg)
+
+## Welcome! 👋
+
+Thanks for checking out this front-end coding challenge.
+
+## Overview
+
+My name is Ajor Moses, I'm open to learning and improvement, Thanks for checking out my front-end coding challenge.
+
+## Screenshot
+
+You will find all the required assets in the `/images` and `/design` folder. The assets are already optimized.
+
+## Links
+
+- [Solution](#Solution) URL: [ https://github.com/ajormoses/calculator-app.git ]
+- [Live_Site_URL](#Live_Site) URL: [ https://ajormoses.github.io/calculator-app/ ]
+
+## Built with
+
+I use HTML, SASS & JAVASCRIPT to build this design
+
+## Author
+
+- [FrontendMentor](#FrontendMentor) - [@ajormoses] [ https://www.frontendmentor.io/profile/ajormoses ]
+- [GithubPage](#Githubpage): [ https://github.com/ajormoses ]
+- [Linkedin](#Linkedin): [ https://www.linkedin.com/in/moses-ajor-0b99291a7/ ]
+- [Twitter](#Twitter): [ https://twitter.com/ajor_moses ]
---
+++ script.js
@@ -0,0 +1,56 @@
+const redBtn = document.querySelector(".red-btn");
+const orangeBtn = document.querySelector(".orange-btn");
+const cyanBtn = document.querySelector(".cyan-btn");
+const display = document.querySelector(".text-body");
+const buttons = Array.from(document.getElementsByClassName("buttons"));
+
+redBtn.addEventListener("click", () => {
+  document.body.classList.add("red-theme");
+  document.body.classList.remove("orange-theme");
+  document.body.classList.remove("cyan-theme");
+  redBtn.style.opacity = "1";
+  orangeBtn.style.opacity = "0";
+  cyanBtn.style.opacity = "0";
+});
+
+orangeBtn.addEventListener("click", () => {
+  document.body.classList.add("orange-theme");
+  document.body.classList.remove("red-theme");
+  document.body.classList.remove("cyan-theme");
+  redBtn.style.opacity = "0";
+  orangeBtn.style.opacity = "1";
+  cyanBtn.style.opacity = "0";
+});
+
+cyanBtn.addEventListener("click", () => {
+  document.body.classList.add("cyan-theme");
+  document.body.classList.remove("red-theme");
+  document.body.classList.remove("orange-theme");
+  redBtn.style.opacity = "0";
+  orangeBtn.style.opacity = "0";
+  cyanBtn.style.opacity = "1";
+});
+
+buttons.map((button) => {
+  button.addEventListener("click", (e) => {
+    switch (e.target.innerText) {
+      case "RESET":
+        display.innerText = "";
+        break;
+      case "DEL":
+        if (display.innerText) {
+          display.innerText = display.innerText.slice(0, -1);
+        }
+        break;
+      case "=":
+        try {
+          display.innerText = eval(display.innerText);
+        } catch {
+          display.innerHTML = "";
+        }
+        break;
+      default:
+        display.innerText += e.target.innerText;
+    }
+  });
+});
